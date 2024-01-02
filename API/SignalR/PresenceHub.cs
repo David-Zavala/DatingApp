3eb241ae7ee62f8c5bcfa4bd945181c4fe9bf2bc
David Zavala <david.zavalal@udem.edu>
2024-01-02 00:07:22 -0600

    End of Section 17

---
+++ API/SignalR/PresenceHub.cs
@@ -0,0 +1,30 @@
+using API.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace API.SignalR
+{
+    [Authorize]
+    public class PresenceHub(PresenceTracker tracker) : Hub
+    {
+        public readonly PresenceTracker _tracker = tracker;
+        public override async Task OnConnectedAsync()
+        {
+            var isOnline = await _tracker.UserConnected(Context.User.GetUsername(), Context.ConnectionId);
+            if (isOnline)
+                await Clients.Others.SendAsync("UserIsOnline", Context.User.GetUsername());
+
+            var currentUsers = await _tracker.GetOnlineUsers();
+            await Clients.Caller.SendAsync("GetOnlineUsers",currentUsers);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            var isOffline = await _tracker.UserDisconnected(Context.User.GetUsername(), Context.ConnectionId);
+            if (isOffline)
+                await Clients.Others.SendAsync("UserIsOffline", Context.User.GetUsername());
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from '../../_models/member';
 import { MembersService } from '../../_services/members.service';
 import { ToastService } from '../../_services/toast.service';
+import { PresenceService } from '../../_services/presence.service';
 
 @Component({
   selector: 'app-member-card',
@@ -11,7 +12,7 @@ import { ToastService } from '../../_services/toast.service';
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
 
-  constructor(private memberService: MembersService, private toast: ToastService) { }
+  constructor(private memberService: MembersService, private toast: ToastService, public presenceService: PresenceService) { }
 
   ngOnInit(): void {
     
